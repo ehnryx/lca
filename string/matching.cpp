4e1ad67d7606151c2f02fa701dde09d37d1bf120
Henry Xia <henryxia9999@gmail.com>
2019-08-18 14:19:03 -0700

    upd

--- string/matching.cpp
+++ string/matching.cpp
@@ -56,6 +56,7 @@ vector<int> kmp(const string& s, const string& t) {
 ////////////////////////////////////////////////////////////////////////
 // Aho-Corasick String Matching -- O(n + m + numMatches)
 // WARNING: all pattern strings must be different; patterns 0-indexed
+//          change `int id` to `vector<int> id` for non-unique patterns
 // USAGE:
 //  0. AhoCorasick trie;
 //  1. trie.insert(pattern) to insert patterns
@@ -63,55 +64,56 @@ vector<int> kmp(const string& s, const string& t) {
 //  3. auto matches = trie.match(string) to find words in string
 //*!
 struct AhoCorasick {
-	struct Node {
-		Node *par, *end, *p;
-		unordered_map<char,Node*> ch;
+    struct Node {
+        Node *par, *end, *p;
+        unordered_map<char,Node*> ch;
         int id;
-		Node(Node* u=0): par(u), id(-1) {}
-	};
+        Node(Node* u=0): par(u), id(-1) {}
+    };
 
-	Node* root;
-	vector<int> len;
-	int wcnt;
-	AhoCorasick(): root(new Node), wcnt(0) {}
+    Node* root;
+    vector<int> len;
+    int wcnt;
+    AhoCorasick(): root(new Node), wcnt(0) {}
 
-	void insert(const string& s) {
-		Node* u = root;
-		for (char c : s) {
-			if (!u->ch.count(c)) u->ch[c] = new Node(u);
-			u = u->ch[c];
-		}
-		assert(u->id==-1); u->id = wcnt++;
-		len.push_back(s.size());
-	}
+    void insert(const string& s) {
+        Node* u = root;
+        for (char c : s) {
+            if (!u->ch.count(c)) u->ch[c] = new Node(u);
+            u = u->ch[c];
+        }
+        assert(u->id==-1); u->id = wcnt++;
+        len.push_back(s.size());
+    }
 
-	void build() {
-		queue<pair<char,Node*>> bfs; bfs.push({0,root});
-		while (!bfs.empty()) {
-			char c; Node* u; tie(c,u) = bfs.front(); bfs.pop();
-			for (const auto& i : u->ch) bfs.push(i);
-			u->p = u->end = root;
-			if (u!=root) {
-				Node* v = u->par->p;
-				while (v!=root && !v->ch.count(c)) v = v->p;
-				if (v->ch.count(c) && v->ch[c]!=u) u->p = v->ch[c];
-				u->end = (u->id==-1 ? u->p->end : u);
-			}
-		}
-	}
+    void build() {
+        queue<pair<char,Node*>> bfs; bfs.push({0,root});
+        while (!bfs.empty()) {
+            char c; Node* u;
+            tie(c,u) = bfs.front(); bfs.pop();
+            for (const auto& i : u->ch) bfs.push(i);
+            u->p = u->end = root;
+            if (u!=root) {
+                Node* v = u->par->p;
+                while (v!=root && !v->ch.count(c)) v = v->p;
+                if (v->ch.count(c) && v->ch[c]!=u) u->p = v->ch[c];
+                u->end = (u->id==-1 ? u->p->end : u);
+            }
+        }
+    }
 
-	vector<vector<int>> match(const string& s) {
-		vector<vector<int>> m(wcnt, vector<int>());
-		Node* u = root;
-		for (int i=0; i<s.size(); i++) {
-			while (u!=root && !u->ch.count(s[i])) u = u->p;
-			if (u->ch.count(s[i])) u = u->ch[s[i]];
-			for (Node* v=u; v->end!=root; v=v->p) {
-				v = v->end; // get matches
+    vector<vector<int>> match(const string& s) {
+        vector<vector<int>> m(wcnt, vector<int>());
+        Node* u = root;
+        for (int i=0; i<s.size(); i++) {
+            while (u!=root && !u->ch.count(s[i])) u = u->p;
+            if (u->ch.count(s[i])) u = u->ch[s[i]];
+            for (Node* v=u; v->end!=root; v=v->p) {
+                v = v->end; // get matches
                 m[v->id].push_back(i-len[v->id]+1);
-			}
-		}
-		return m;
+            }
+        }
+        return m;
     }
 };
 //*/

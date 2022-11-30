8ad495943f279d218d2e1d90284940af8e3e341b
Henry X <henryxia9999@gmail.com>
2022-11-29 23:05:25 -0600

    stuff3

--- graph/flow_with_demands.h
+++ graph/flow_with_demands.h
@@ -1,4 +1,4 @@
-/* Flow with demands (Circulation)
+/* dinic with demands (Circulation)
  * USAGE
  *  flow_with_demands<flow_class> graph(n);
  *  graph.add_edge(a, b, demand, capacity);
@@ -17,17 +17,19 @@
  */
 #pragma once
 
+#include "dinic.h"
+
 #include <numeric>
 #include <stdexcept>
 #include <vector>
 
-template <template <typename> class Flow, typename T>
-struct flow_with_demands : Flow<T> {
+template <typename T>
+struct flow_with_demands : dinic<T> {
   int source, sink;
   std::vector<T> in, out;
   T saturated;
   flow_with_demands(int n):
-    Flow<T>(n + 2), source(n), sink(n + 1), in(n), out(n), saturated(0) {}
+    dinic<T>(n + 2), source(n), sink(n + 1), in(n), out(n), saturated(0) {}
 
   void add_edge(int a, int b, const T& d, const T& c) {
     if (a >= source or b >= source) {
@@ -37,7 +39,7 @@ struct flow_with_demands : Flow<T> {
       throw std::invalid_argument("add_edge: demand is larger than capacity");
     }
     if (c != d) {
-      Flow<T>::add_edge(a, b, c - d);
+      dinic<T>::add_edge(a, b, c - d);
     }
     in[b] += d;
     out[a] += d;
@@ -46,11 +48,11 @@ struct flow_with_demands : Flow<T> {
   void finalize() {
     for (int i = 0; i < source; i++) {
       if (in[i] != 0) {
-        Flow<T>::add_edge(source, i, in[i]);
+        dinic<T>::add_edge(source, i, in[i]);
         saturated += in[i];
       }
       if (out[i] != 0) {
-        Flow<T>::add_edge(i, sink, out[i]);
+        dinic<T>::add_edge(i, sink, out[i]);
       }
     }
   }
@@ -59,17 +61,17 @@ struct flow_with_demands : Flow<T> {
     if (s >= source or t >= source) {
      throw std::invalid_argument("flowable: node indices out of bounds");
     }
-    Flow<T>::add_edge(t, s, max_flow);
-    T res = (Flow<T>::flow(source, sink) == saturated ? Flow<T>::adj[t].back().flow : -1);
-    Flow<T>::adj[s].pop_back();
-    Flow<T>::adj[t].pop_back();
+    dinic<T>::add_edge(t, s, max_flow);
+    T res = (dinic<T>::flow(source, sink) == saturated ? dinic<T>::adj[t].back().flow : -1);
+    dinic<T>::adj[s].pop_back();
+    dinic<T>::adj[t].pop_back();
     return res;
   }
 
   T min_flowable(int s, int t) {
     T res = flowable(s, t);
     if (res != -1) {
-      res -= Flow<T>::flow(t, s);
+      res -= dinic<T>::flow(t, s);
     }
     return res;
   }
--- graph/push_relabel.h
+++ graph/push_relabel.h
@@ -17,29 +17,33 @@
  */
 #pragma once
 
+#include <numeric>
+#include <queue>
+#include <vector>
+
 template <typename T>
 struct push_relabel {
-  static constexpr T inf = numeric_limits<T>::max();
+  static constexpr T inf = std::numeric_limits<T>::max();
   struct edge {
     int to, rev;
     T cap, flow;
     edge(int t, int r, const T& c, const T& f): to(t), rev(r), cap(c), flow(f) {}
   };
-  int n;
-  vector<vector<edge>> adj;
-  vector<vector<int>> active;
-  vector<int> height, layer;
-  vector<T> excess;
+  std::vector<std::vector<edge>> adj;
+  std::vector<std::vector<int>> active;
+  std::vector<int> height, layer;
+  std::vector<T> excess;
   int work;
-  push_relabel(int _n): n(_n), adj(n), active(n), height(n), layer(n), excess(n) {}
+  push_relabel(int n): adj(n), active(n), height(n), layer(n), excess(n) {}
   void add_edge(int a, int b, const T& c, bool bidirectional = false) {
-    adj[a].emplace_back(b, (int)size(adj[b]), c, 0);
-    adj[b].emplace_back(a, (int)size(adj[a]) - 1, bidirectional ? c : 0, 0);
+    adj[a].emplace_back(b, (int)adj[b].size(), c, 0);
+    adj[b].emplace_back(a, (int)adj[a].size() - 1, bidirectional ? c : 0, 0);
   }
-  bool left_of_min_cut(int u) const { return height[u] >= n; }
+  bool left_of_min_cut(int u) const { return height[u] >= size(); }
+  int size() const { return (int)adj.size(); }
   void clear_flow() {
-    active.resize(n);
-    for (int i = 0; i < n; i++) {
+    active.resize(size());
+    for (int i = 0; i < size(); i++) {
       for (edge& e : adj[i]) {
         e.flow = 0;
       }
@@ -50,13 +54,13 @@ struct push_relabel {
 
   int global_relabel(int sink, int cur_height = -1) {
     work = 0;
-    fill(begin(height), end(height), n);
+    fill(begin(height), end(height), size());
     fill(begin(layer), end(layer), 0);
     for (int i = 0; i <= cur_height; i++) {
       active[i].clear();
     }
     int max_height = 0;
-    queue<int> bfs;
+    std::queue<int> bfs;
     bfs.push(sink);
     height[sink] = 0;
     layer[0] += 1;
@@ -64,7 +68,7 @@ struct push_relabel {
       int u = bfs.front();
       bfs.pop();
       for (edge& e : adj[u]) {
-        if (height[e.to] == n && adj[e.to][e.rev].flow < adj[e.to][e.rev].cap) {
+        if (height[e.to] == size() && adj[e.to][e.rev].flow < adj[e.to][e.rev].cap) {
           bfs.push(e.to);
           height[e.to] = height[u] + 1;
           layer[height[e.to]] += 1;
@@ -91,9 +95,9 @@ struct push_relabel {
 
   int discharge(int u, bool backward = false) {
     int max_to = height[u];
-    while (excess[u] > 0 && (backward || height[u] < n)) {
+    while (excess[u] > 0 && (backward || height[u] < size())) {
       work++;
-      int relabel_height = backward ? 2 * n : n;
+      int relabel_height = backward ? 2 * size() : size();
       for (edge& e : adj[u]) {
         if (e.flow == e.cap) continue;
         if (height[e.to] >= height[u]) {
@@ -108,19 +112,19 @@ struct push_relabel {
       }
       if (excess[u] == 0) break;
       if (!backward && layer[height[u]] == 0) {
-        height[u] = n; // disconnected from the sink
+        height[u] = size(); // disconnected from the sink
       } else {
         height[u] = relabel_height;
       }
     }
-    if (height[u] < n) {
+    if (height[u] < size()) {
       layer[height[u]] += 1;
     }
     return max_to;
   }
 
   T calc_flow(int source, int sink, int work_before_relabel) {
-    height[source] = n;
+    height[source] = size();
     excess[source] = inf;
     excess[sink] = -inf;
     for (edge& e : adj[source]) {
@@ -128,14 +132,14 @@ struct push_relabel {
         push(source, e);
       }
     }
-    layer[0] = n - 1;
+    layer[0] = size() - 1;
     for (int cur = work = 0; cur >= 0; cur--) {
       while (!empty(active[cur])) {
         int u = active[cur].back();
         active[cur].pop_back();
         layer[cur] -= 1;
         cur = discharge(u);
-        if (work > work_before_relabel * n) {
+        if (work > work_before_relabel * size()) {
           cur = global_relabel(sink, cur);
         }
       }
@@ -147,14 +151,14 @@ struct push_relabel {
     if (!need_flow) return calc_flow(source, sink, work_before_relabel);
     T res = calc_flow(source, sink, work_before_relabel);
     global_relabel(sink);
-    active.resize(2 * n);
+    active.resize(2 * size());
     excess[source] = -inf;
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < size(); i++) {
       if (i != source && excess[i] > 0) {
-        active[n].push_back(i);
+        active[size()].push_back(i);
       }
     }
-    for (int cur = n; cur >= n; cur--) {
+    for (int cur = size(); cur >= size(); cur--) {
       while (!empty(active[cur])) {
         int u = active[cur].back();
         active[cur].pop_back();
